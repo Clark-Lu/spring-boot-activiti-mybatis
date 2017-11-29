4697edfff7883cb6d862a3cfc3a7cbbee46ababf
Clark-Lu <31913218+Clark-Lu@users.noreply.github.com>
2017-11-29 21:59:30 +0800

    get all url

--- src/main/java/com/clark/springbootactivitidemo/controller/LoginController.java
+++ src/main/java/com/clark/springbootactivitidemo/controller/LoginController.java
@@ -1,14 +1,26 @@
 package com.clark.springbootactivitidemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Controller(value = "/")
 public class LoginController {
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    @RequestMapping(value = {"/login","/url1","url2"},method = RequestMethod.GET)
     public String login(@RequestParam String user ,@RequestParam String password){
         return "login";
     }
+
 }
+
+
